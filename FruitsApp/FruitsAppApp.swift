4256ae7612289f2054bfad19c6d5d5a7d1a200dc
Brandon Taylor <taybrandon@gmail.com>
2022-06-03 12:43:24 -0500

    Initial Commit

---
+++ FruitsApp/FruitsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsAppApp.swift
+//  FruitsApp
+//
+//  Created by Brandon Taylor on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct FruitsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
